b878b004268359f12925ee66b3ba74edad26053a
oleetech <rebelsoft111122@gmail.com>
2024-08-11 15:07:51 +0600

    add mongodb code

--- src/config/database.js
+++ src/config/database.js
@@ -6,18 +6,33 @@ const { DataSource } = require('typeorm');
 // Determine the database type from environment variables
 const dbType = process.env.DB_TYPE || 'mysql';
 
-const AppDataSource = new DataSource({
-    type: dbType, // Use the database type specified in the .env file
-    host: process.env.DB_HOST || 'localhost',
-    port: parseInt(process.env.DB_PORT, 10) || 3306,
-    username: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_DATABASE || 'passportjs_example',
+// Common configuration options
+const commonConfig = {
     synchronize: process.env.DB_SYNCHRONIZE === 'true',
     logging: process.env.DB_LOGGING === 'true',
     entities: [], // Add your entities here or load from a specific file
     migrations: [], // Add your migrations here or load from a specific file
     subscribers: [], // Add your subscribers here or load from a specific file
+};
+
+// Conditional configuration based on DB type
+const dbConfig = dbType === 'mongodb' ? {
+    type: 'mongodb',
+    url: `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`, // MongoDB connection string
+    useNewUrlParser: true, // MongoDB-specific options
+    useUnifiedTopology: true, // MongoDB-specific options
+} : {
+    type: dbType,
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT, 10) || 3306,
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_DATABASE || 'passportjs_example',
+};
+
+const AppDataSource = new DataSource({
+    ...commonConfig,
+    ...dbConfig,
 });
 
 AppDataSource.initialize()
